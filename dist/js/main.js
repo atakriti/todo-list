865f2fc121f1b2e783b574bcd8d7b6db8c556999
atakriti <anwart256@gmail.com>
2022-06-04 18:09:47 +0200

    done

---
+++ dist/js/main.js
@@ -0,0 +1,152 @@
+// each ul li button X3 ********************** Note
+
+// let p = document.querySelector("p");
+let ul = document.querySelector(".listItem");
+let yetParagraph = document.querySelector(".yet");
+let finishedItems = document.querySelector(".finishedItems");
+let recycleUl = document.querySelector(".recycle");
+let reset = document.querySelector("#reset");
+let emptyTrash = document.querySelector(".emptyTrash");
+let pop = document.querySelector(".pop-container");
+let yes = document.getElementById("yes");
+let no = document.getElementById("no");
+let body = document.querySelector("body");
+// ================== display and POP none for the trash =================
+emptyTrash.style.display = "none";
+pop.style.transform = "scale(0)";
+// =============================================
+// =======================clock ============
+// let hours = new Date().getHours();
+// let minutes = new Date().getMinutes();
+// let time = `${hours}:${minutes}`;
+// clock.innerHTML = time;
+// ============================
+let user = (e) => {
+  e.preventDefault();
+  let inputUser = document.querySelector("#input-user").value;
+  if (!inputUser == "") {
+    let li = document.createElement("li");
+    let spanLi = document.createElement("span");
+    let list = document.createTextNode(inputUser);
+    spanLi.appendChild(list);
+    li.appendChild(spanLi);
+    ul.appendChild(li);
+
+    // let clone = li.cloneNode(true)
+    // ul2.appendChild(clone)
+    document.querySelector("#input-user").value = "";
+    // step 1 create div for btns
+    let div = document.createElement("div");
+    // step 2 create btns
+    let done = document.createElement("button");
+    let remindMe = document.createElement("button");
+    let recycleBtn = document.createElement("button");
+    let trash = document.createElement("button");
+    let restore = document.createElement("button");
+    // step 3 create the content of btns
+    let doneContent = document.createTextNode("✅");
+    let remindMeContent = document.createTextNode("⏰");
+    let recycleContent = document.createTextNode("🗑️");
+    let trashContent = document.createTextNode("❌");
+    let restoreContent = document.createTextNode("🔃");
+    // step4 push content to btns
+    done.appendChild(doneContent);
+    remindMe.appendChild(remindMeContent);
+    recycleBtn.appendChild(recycleContent);
+    trash.appendChild(trashContent);
+    restore.appendChild(restoreContent);
+    // step 5 push btns to div
+    div.appendChild(done);
+    div.appendChild(remindMe);
+    div.appendChild(recycleBtn);
+    div.appendChild(trash);
+    div.appendChild(restore);
+    // step 6 push div to li
+    li.appendChild(div);
+
+    // =============
+
+    // =================== DONE =======================
+    done.addEventListener("click", () => {
+      li.classList.add("done");
+      finishedItems.appendChild(li);
+    });
+    // =================== Remind Me =======================
+    remindMe.addEventListener("click", () => {
+      li.classList.remove("done");
+      li.classList.add("wainting");
+      yetParagraph.appendChild(li);
+    });
+    // =================== recycleBtn =======================
+    recycleBtn.addEventListener("click", () => {
+      // li.remove()
+      li.classList.remove("done");
+      li.classList.add("recycleBg");
+      recycleUl.appendChild(li);
+      emptyTrash.style.display = "block";
+
+      // recycleUl.appendChild(recycleUl);
+    });
+
+    trash.addEventListener("click", () => {
+      li.remove();
+      li.classList.remove("done");
+    });
+
+    reset.addEventListener("click", () => {
+      // li.remove()
+      emptyTrash.style.display = "none";
+      pop.style.transform = "scale(1)";
+      body.classList.add("darkBg");
+      pop.style.animationName = "popUp";
+      pop.style.animationDuration = "1s";
+    });
+
+    emptyTrash.addEventListener("click", () => {
+      let allRecycleLi = document.querySelectorAll(".recycle li");
+      allRecycleLi.forEach((n) => n.remove());
+    });
+
+    restore.addEventListener("click", () => {
+      ul.appendChild(li);
+    });
+    // =========== yes no btn ==========
+    yes.addEventListener("click", () => {
+      li.remove();
+      pop.style.transform = "scale(0)";
+      // body.style.backgroundColor ="white"
+      body.classList.remove("darkBg");
+      pop.style.animationName = "popDown";
+      pop.style.animationDuration = "0.5s";
+    });
+    no.addEventListener("click", () => {
+      pop.style.transform = "scale(0)";
+      // body.style.backgroundColor ="white"
+      body.classList.remove("darkBg");
+      pop.style.animationName = "popDown";
+      pop.style.animationDuration = "0.5s";
+    });
+  } else {
+    document.querySelector("#input-user").placeholder = "Write your toDo list";
+  }
+};
+let submit = document
+  .querySelector(".mainForm")
+  .addEventListener("submit", user);
+
+// ===============
+let userWelcome = document.querySelector(".userWelcome");
+let userWel = (e) => {
+  e.preventDefault();
+  let userWelInput = document.getElementById("userWelInput").value;
+  if (!userWelInput == "") {
+    userWelcome.innerHTML =
+      userWelInput[0].toUpperCase() + userWelInput.slice(1);
+    document.getElementById("userWelInput").value = "";
+  } else {
+    document.getElementById("userWelInput").placeholder = "Enter your name";
+  }
+};
+let userWelcomeForm = document
+  .querySelector(".userWelcomeInput")
+  .addEventListener("submit", userWel);
